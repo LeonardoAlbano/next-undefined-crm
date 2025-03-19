53e7fa62671a6f8d0a3eb6c293f8ff18cff9a3c2
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2025-03-19 02:30:08 -0300

    config

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -1,19 +1,13 @@
-import PostgresAdapter from "@auth/pg-adapter";
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-import { Pool } from "pg";
+"use server";
  
-const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
+ import { auth, signIn, signOut } from "@/lib/auth";
  
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PostgresAdapter(pool),
-  providers: [Google],
-});
+ export async function manageAuth() {
+   const session = await auth();
+ 
+   if (!session) {
+     return await signIn("google", { redirectTo: "/criar" });
+   }
+ 
+   return await signOut({ redirectTo: "/" });
+ }
---
+++ src/http/sign-in-with-google.ts
@@ -0,0 +1,25 @@
+import { api } from "./api-client";
+
+interface SignInWithGoogleParams {
+  token: string
+}
+
+interface SignInWithGoogleResponse {
+  email: string
+  name: string
+  accessToken: string
+}
+
+export async function signInWithGoogle({ token }: SignInWithGoogleParams) {
+  const response = await api
+    .post("users/oauth/google", {
+      json: {
+        token,
+      },
+    })
+    .json<SignInWithGoogleResponse>();
+
+  return {
+    token: response.accessToken,
+  };
+}
